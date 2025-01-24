03e800a06e4fd81913a83acd91811a98badd769b
Marin Bianca <marin.biancaioana@gmail.com>
2025-01-24 17:13:51 -0500

    added add to cart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from "./CartSlice";
 function ProductList() {
     const [addedToCart, setAddedToCart] = useState(false);
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -247,6 +250,15 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, 
+     }));
+  };
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
